1aabb23c8529844baa4ce92e89375a115e60ee1f
EnesT51 <89782456+EnesT51@users.noreply.github.com>
2021-11-09 16:32:49 +0100

    robotarm

--- duizelig/Randomgetal.py
+++ duizelig/Randomgetal.py
@@ -7,10 +7,25 @@ while game:
         
 
         while kansen_10 >1:
+
             kansen_10 -=1
-            print("je hebt nog" ,kansen_10,"kans!")
-            raden =(input("raad het getal tussen 1 en de 1000 "))
-            game = False
+            raden = (input("raad het getal tussen 1 en de 1000, of als je wil stoppen type stop "))
+            
+            if raden == "stop":
+                print("je hebt de game gestopt")
+                game = False
+                kansen_10 = 1
+
+            elif int(raden) == number:
+                print("je hebt de juiste antwoord geraden!!")
+                
+            
+                
+            else:
+                print("je hebt het fout probeer nogmaals!")
+                print("je hebt nog" ,kansen_10,"pogingen!")
+            
+
             
 
         
