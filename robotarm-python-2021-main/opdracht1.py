9a800f2ee99ee282a03c4bc3962c66d774d46920
EnesT51 <99040669@mydavinci.nl>
2021-11-22 14:00:46 +0100

    opdracht12.py

--- robotarm-python-2021-main/opdracht1.py
+++ robotarm-python-2021-main/opdracht1.py
@@ -3,7 +3,7 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.moveRight();
+robotArm.moveRight()
 robotArm.grab()
 robotArm.moveLeft()
 robotArm.drop()
